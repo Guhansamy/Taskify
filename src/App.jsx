a1c2cf7022de0c7368b1052e97a0660350751ea7
Guhan-K <guhan.k2022cse@sece.ac.in>
2024-03-12 22:38:49 +0530

    Body created

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Body from './components/Body'
 import {CreateForm} from './components/CreateForm'
+import Taskspage from './components/Taskspage'
 
 function App() {
 
   return (
     <>
       <Header />
-      <Body/>
-      {/* <CreateForm/> */}
-      
-      {/* <Header /> */}
       {/* <Body/> */}
-      {/* <CreateForm/> */}
-      <Taskspage/>
+      <CreateForm/>
+      {/* <Taskspage/> */}
     </>
   )
 }
