e7015b0d43ac3db833b398b66ecc76718bf4cd2f
dprakash22 <deepakprakash.s2022cse@sece.ac.in>
2024-03-12 21:49:35 +0530

    commit taskspage and Addtaskpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Body from './components/Body'
 import {CreateForm} from './components/CreateForm'
+import Taskspage from './components/Taskspage'
 
 function App() {
 
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       {/* <Body/> */}
-      <CreateForm/>
+      {/* <CreateForm/> */}
+      <Taskspage/>
     </>
   )
 }
---
+++ src/components/Taskspage.jsx
@@ -0,0 +1,45 @@
+import Addtask from "./Addtask"
+import "../assets/deepak.css"
+
+const Taskspage=()=>{
+    return (
+        <>
+        <div className="whole">
+            <div className="sidepage">
+               
+                 <div className="top-sidepage">
+                    <div className="circle">
+                        <div className="side-logo">D</div>
+                    </div>
+                    <h3 className="borderbox">Deepak</h3>           
+                </div>
+
+                <div className="sidebottom">
+                    <h4 className="borderbox">Classcode</h4>
+                    <h4 className="borderbox">Members - 60</h4>
+                    
+                </div>
+            </div>
+
+              <div className="rightside">
+                
+                <div className="titlecard">
+                    <h1 style={{"position":"absolute","marginLeft":"60px","marginTop":"60px"}}> Discrete Mathematics</h1>
+                    <img style={{"position":"absolute","width":"290px","marginLeft":"600px"}} src="./src/assets/book1.png"></img>
+                </div>
+                
+                <div className="tasklist">
+                    <Addtask/>
+                    <Addtask/>
+                    <Addtask/>
+                    <Addtask/>
+                    <Addtask/>
+                    <Addtask/>
+                    <Addtask/>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Taskspage
