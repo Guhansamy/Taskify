15132819f33dfd18b6da6d80ca81c5930002769d
Guhan-K <guhan.k2022cse@sece.ac.in>
2024-03-12 20:58:00 +0530

    Header and body created

---
+++ src/components/Body.jsx
@@ -0,0 +1,48 @@
+import "../assets/styles.css"
+
+const Body = () => {
+    return (
+        <>
+            <div className="body-main">
+
+                <div className="body-first"> 
+                    <div className="body-name">
+                        Hey Deepa you have 15 task remaining
+                    </div>
+                    <div>
+                        <button className="body-join-button" > Join Task</button>
+                    </div>
+                </div>
+
+                <div className="body-second"> 
+                    <div className="square-1">
+                        <div className="square-circle"> </div>
+                        <div className="square-assign">
+                            <span> Test</span>
+                        </div>
+                        <div className="square-par">
+                            <div> <img src="https://cdn-icons-png.flaticon.com/128/14607/14607522.png" className="par-img"/>  </div>
+                            <div className="par-num"> 56 </div>
+                        </div>
+
+                    </div>
+                    <div className="square-1"></div>
+                    <div className="square-1"></div>
+                    <div className="square-1"></div>
+                    <div className="square-1"></div>
+                    <div className="square-1"></div>
+                    <div className="square-1"></div>
+                    <div className="square-1"></div>
+                    <div className="square-1"></div>
+                    <div className="square-1"></div>   
+                    <div className="square-1"></div> 
+                    <div className="square-1"></div> 
+                    <div className="square-1"></div> 
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Body;
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import "../assets/styles.css"
+
+const Header = () => {
+
+    return (
+        <>
+           {/* <div className="main-header"> */}
+
+                <div className="first-header"> 
+                
+                    <div className="first-1"> 
+                        <div>logo</div>
+                    </div>
+
+                    <div className="first-2">
+                        <div>Home</div>
+                        <div>Current</div>
+                        <div>Profile</div>
+                    </div>
+                </div>
+           {/* </div>    */}
+        </>
+    )
+}
+
+export default Header
