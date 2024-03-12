f44f1dc2fa94a9bf35a395ef12589aa540ee696d
dharun <dharunpalanisamy5256@gmail.com>
2024-03-12 21:32:17 +0530

    changed dharun.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './dharun.css'
+import './assets/dharun.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
