d07f6e71a1ee290579576bfbd7d3bd85dcf5a800
Guhan-K <guhan.k2022cse@sece.ac.in>
2024-03-14 00:13:46 +0530

    barbie

--- src/components/Taskspage.jsx
+++ src/components/Taskspage.jsx
@@ -2,8 +2,15 @@ import { Outlet } from "react-router"
 import "../assets/deepak.css"
 import { CreateForm } from "./CreateForm"
 import { TaskList } from "./TaskList"
+import { useState } from "react"
 
 const Taskspage=()=>{
+
+    const[logos , setLogos] = useState()
+    cosnt[names , setNames] = useState()
+    const[clscode , setClsCode] = useState()
+    const[membrs , setMembrs] = useState();
+
     return (
         <>
         <div className="whole">
